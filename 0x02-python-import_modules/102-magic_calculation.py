90316a350f05b0354e04f9f260fe3bc12ba25f51
waruguru-lewis-gatimu <warugurulewis@gmail.com>
2023-11-30 06:09:57 -0800

    import and modules

---
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+if _name_ = "_main_":
+    """displaying the components of my function 102-magic_calculation.py"""
+    import calculator_1
+    import sys
+    def magic_calculation(a, b):
+        dis.dis(def magic_calculation(a, b))
